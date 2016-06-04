02c736b7455516e6ffad047fdafbcdf4fc00d2d8
Chibuzor Alumba <coolchibs@gmail.com>
2016-06-04 17:01:48 -0500

    Completed the delete functionality of the todo app

--- app/routes/todo-list.js
+++ app/routes/todo-list.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 var todoList = [
-	{text:'Sample Todo 1', children: []},//[{text:'Todo Child 1', children: []}]},
-	{text:'Sample Todo 2', children: []},
-	{text:'Sample Todo 3', children: []}
+	
 ];
 
 export default Ember.Route.extend({
