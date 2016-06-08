06d7c1794ee16a1df8349ba944e077bbd01a62eb
Chibuzor Alumba <coolchibs@gmail.com>
2016-06-08 18:42:30 -0500

    Pressing Shift+Tab now makes a child into its parent's sibling

--- app/components/edit-todo-view.js
+++ app/components/edit-todo-view.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	didInsertElement: function() {
     this.$('input').focus();
-    var self = this,
-			ctx = this.$('input').keydown(function (event) {
-				if (event.which === 9) {
-					// [TAB]
-					event.preventDefault();
-					self.get('target').send('handleKeyPress', event);
-				}
-			});
+    var self = this;
+		this.$('input').keydown(function (event) {
+			if (event.which === 9) {
+				// [TAB]
+				event.preventDefault();
+				self.get('target').send('handleKeyPress', event);
+			}
+		});
   }
 });
 
